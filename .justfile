e1805afd5744170562862f7a55fb20ea041b1fc1
Brooke <brooke@gril.me>
2023-05-27 21:20:06 +0100

    Initial commit

---
+++ .justfile
@@ -0,0 +1,13 @@
+analyze: install-packages
+    rojo sourcemap default.project.json --output sourcemap.json
+    curl -O https://raw.githubusercontent.com/JohnnyMorganz/luau-lsp/main/scripts/globalTypes.d.lua
+    luau-lsp analyze --definitions=globalTypes.d.lua --base-luaurc=.luaurc --sourcemap=sourcemap.json src/
+
+# Installs packages and proxies their type information with `wally-package-types` tool
+# In addition, the packages/ directory is temporarily renamed so that it isn't removed by Wally
+install-packages:
+    wally install
+
+    rojo sourcemap packages.project.json --output sourcemap.json
+    wally-package-types --sourcemap sourcemap.json Packages/
+
---
+++ src/init.lua
@@ -0,0 +1,39 @@
+-- ROBLOX note: no upstream
+--[[
+	* Copyright (c) Roblox Corporation. All rights reserved.
+	* Licensed under the MIT License (the "License");
+	* you may not use this file except in compliance with the License.
+	* You may obtain a copy of the License at
+	*
+	*     https://opensource.org/licenses/MIT
+	*
+	* Unless required by applicable law or agreed to in writing, software
+	* distributed under the License is distributed on an "AS IS" BASIS,
+	* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	* See the License for the specific language governing permissions and
+	* limitations under the License.
+]]
+local Components = script.Components
+local Lists = script.Lists
+local ScrollView = require(Components.ScrollView.ScrollView)
+local VirtualizedList = require(Lists.VirtualizedList)
+local SectionList = require(Lists.SectionList).default
+local FlatList = require(Lists.FlatList)
+local View = require(Components.View.View)
+local ViewabilityHelper = require(Lists.ViewabilityHelper)
+local Hooks = require(Lists.Hooks)
+local AnimatedFlatList = require(Lists.AnimatedFlatList)
+
+export type ViewabilityConfigCallbackPair = ViewabilityHelper.ViewabilityConfigCallbackPair
+export type ViewabilityConfig = ViewabilityHelper.ViewabilityConfig
+export type AnimatedFlatListProps<T> = AnimatedFlatList.Props<T>
+
+return {
+	ScrollView = ScrollView,
+	VirtualizedList = VirtualizedList,
+	SectionList = SectionList,
+	FlatList = FlatList,
+	View = View,
+	AnimatedFlatList = AnimatedFlatList,
+	Hooks = Hooks,
+}
